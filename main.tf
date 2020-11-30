cf3a7f4066ed1abd6f8dba5a205f0e90480ba712
Dave Hill <dave@davehill.org>
2020-11-30 19:07:42 +0000

    Possible typo in subnet

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_subnet" "us-west-2a" {
 
 resource "aws_instance" "us-west-2-ec2" {
 
-  subnet_id              = "aws_subnet.us-west-2a.id"
+  subnet_id              = aws_subnet.us-west-2a.id
   ami                    = "ami-0c5204531f799e0c6"
   instance_type          = "t2.micro"
 
