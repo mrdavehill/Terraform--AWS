61e87bd9c0d52335d58019f3a5fa90dd2cdf8560
Dave Hill <dave@davehill.org>
2020-12-01 14:01:16 +0000

    Working draft

--- main.tf
+++ main.tf
@@ -18,9 +18,12 @@ resource "aws_vpc" "us-west-2-vpc" {
 }
 
 resource "aws_subnet" "us-west-2a" {
-  vpc_id            = aws_vpc.us-west-2-vpc.id
-  cidr_block        = "10.0.0.0/27"
-  availability_zone = "us-west-2a"
+  vpc_id                  = aws_vpc.us-west-2-vpc.id
+  cidr_block              = "10.0.0.0/27"
+  availability_zone       = "us-west-2a"
+  map_public_ip_on_launch = "true"
+
+  depends_on              = [aws_internet_gateway.us-west-2-igw]
 }
 
 
@@ -30,6 +33,8 @@ resource "aws_instance" "us-west-2-ec2" {
   ami                    = "ami-0c5204531f799e0c6"
   instance_type          = "t2.micro"
 
+  security_groups = [aws_security_group.ssh.id, aws_security_group.icmp.id]
+
   key_name               = "aws_ssh_key_aws.eveldave"
 
   tags = {
@@ -38,8 +43,70 @@ resource "aws_instance" "us-west-2-ec2" {
   }
 }
 
-resource "aws_internet_gateway" "gw" {
+resource "aws_internet_gateway" "us-west-2-igw" {
   vpc_id = aws_vpc.us-west-2-vpc.id
+}
+
+resource "aws_route_table" "us-west-2-rt" {
+  vpc_id = aws_vpc.us-west-2-vpc.id
+  
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.us-west-2-igw.id
+  }
+}
+
+resource "aws_route_table_association" "us-west-2a-rta-subnet" {
+  subnet_id      = aws_subnet.us-west-2a.id
+  route_table_id = aws_route_table.us-west-2-rt.id
+}
+
+resource "aws_security_group" "icmp" {
+  name                = "allow ICMP"
+  vpc_id              = aws_vpc.us-west-2-vpc.id 
+  ingress {
+      from_port       = 0
+      to_port         = 0
+      protocol        = "icmp"
+      cidr_blocks     = ["0.0.0.0/0"]
+    }
+
+  egress {
+      from_port       = 0
+      to_port         = 0
+      protocol        = "-1"
+      cidr_blocks     = ["0.0.0.0/0"]
+    }
+
+  lifecycle {
+      create_before_destroy = true
+    }
+}
+
+resource "aws_security_group" "ssh" {
+  name                = "allow SSH"
+  vpc_id              = aws_vpc.us-west-2-vpc.id
+    ingress {
+      from_port       = 22
+      to_port         = 22
+      protocol        = "tcp"
+      cidr_blocks     = ["0.0.0.0/0"]
+    }
+
+    egress {
+      from_port      = 0
+      to_port        = 0
+      protocol       = "-1"
+      cidr_blocks    = ["0.0.0.0/0"]
+    }
+
+    lifecycle {
+        create_before_destroy = true
+        }
+}
+
+output "public_ip" {
+  value = aws_instance.us-west-2-ec2.public_ip
 }
 
 
